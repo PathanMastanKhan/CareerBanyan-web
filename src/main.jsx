869a35d2ab3f78b88f5bfd4a7024151054277e25
Pathan Mastan Khan <102128839+PathanMastanKhan@users.noreply.github.com>
2026-07-30 06:54:57 +0530

    Update imports and rendering in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,6 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx';
 import { Analytics } from '@vercel/analytics/react';
-
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
